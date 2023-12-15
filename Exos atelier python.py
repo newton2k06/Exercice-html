8458130a0b3bb55c1eec577afd9adf093f6b6f48
newton2k06 <152610683+newton2k06@users.noreply.github.com>
2023-12-15 23:10:44 +0100

    Add files via upload

---
+++ Exos atelier python.py
@@ -0,0 +1,11 @@
+from math import*
+
+print("Bonjour!")
+print("On va calculer le volume d'une sphere ")
+
+rayon = int(input("quel est le rayon"))
+
+a=4/3
+b= rayon**3
+V= a*b*pi
+print(f"Le volume de la sphere est {V} ")
